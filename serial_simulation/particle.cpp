e85490943e965bb4bfdb63633a1720b3423a3bdc
lighteningAB <124469718+lighteningAB@users.noreply.github.com>
2025-03-03 12:45:23 +0000

    mostly done particle

--- serial_simulation/particle.cpp
+++ serial_simulation/particle.cpp
@@ -1,8 +1,53 @@
 #include "particle.h"
 #include <iostream>
+#include <cmath>
 
-Particle::Particle(double m, double* p, double* v){
+Particle::Particle(double m, double *p, double *v, double *walls)
+{
     mass = m;
-    std::copy(p, p+3, pos);
-    std::copy(v, v+3, vel);
+    std::copy(p, p + 3, pos);
+    std::copy(v, v + 3, vel);
+    std::copy(walls, walls + 3, boundary);
+}
+
+void Particle::updatePos(double dt)
+{
+    pos[0] += vel[0]; // normal coord updates
+    pos[1] += vel[1];
+    pos[2] += vel[2];
+
+    for (int i = 0; i < 3; i++) // bounding box reflection
+    {
+        if (pos[i] < 0)
+        {
+            pos[i] *= -1.0;
+            vel[i] = abs(vel[i]);
+        }
+        if (pos[i] > boundary[i])
+        {
+            pos[i] = 2 * boundary[i] - pos[i];
+            vel[i] = -1.0 * abs(vel[i]);
+        }
+    }
+}
+
+void Particle::updateVel(double inflM, double *inflPos)
+{
+    
+}
+
+double Particle::getSigma(double inflM)
+{
+    double returnable;
+    mass != inflM ? returnable = 2.0 : mass == 1.0 ? returnable = 1.0
+                                                   : returnable = 3.0;
+    return returnable;
+}
+
+double Particle::getEpsilon(double inflM)
+{
+    double returnable;
+    mass != inflM ? returnable = 15.0 : mass == 1.0 ? returnable = 3.0
+                                                    : returnable = 60.0;
+    return returnable;
 }
--- serial_simulation/particle.h
+++ serial_simulation/particle.h
@@ -7,16 +7,17 @@ public:
     double mass;   // particle's mass
     double pos[3]; // particle's position
     double vel[3]; // particle's velocity
+    double boundary[3]; //bounding box
 
-    Particle(double m, double *pos, double *vel); // initializor
+    Particle(double m, double *pos, double *vel, double* boundary); // initializor
 
     void updatePos(double dt); // change position of particle
 
-    void updateVel(double inflM, double inflPos); //
+    void updateVel(double inflM, double* inflPos); // update velocity based on one other particle
 
-    double getSigma(double inflM);
+    double getSigma(double inflM); // get sigma (size) based on influencing particle
 
-    double getEpsilon(double inflM);
+    double getEpsilon(double inflM); // get epsilon (energy) based on influecning particle
 };
 
 #endif
